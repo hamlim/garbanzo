b553e9f7fe6e7438a0af14363ac2095422183953
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-02-17 10:11:11 -0500

    Add hono context

--- apps/template/src/app/api/greet/route.ts
+++ apps/template/src/app/api/greet/route.ts
@@ -1,3 +1,9 @@
-export default async function handler(request: Request) {
+import { getHonoContext } from "#utils/get-hono-context";
+
+export default async function handler(_request: Request) {
+  let ctx = getHonoContext();
+
+  // if ctx is null, we're in the build phase of the app
+  console.log(ctx?.req.header("User-Agent"));
   return new Response("Hello, world!");
 }
